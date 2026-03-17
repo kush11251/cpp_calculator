87796ac5659f04d75e94c807fb4ee7d4f6280d21
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 00:51:22 +0530

    feat: integrate src/calculator.hpp

---
+++ src/calculator.hpp
@@ -0,0 +1,13 @@
+// calculator.hpp
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+class Calculator {
+public:
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    int divide(int a, int b);
+};
+
+#endif  // CALCULATOR_HPP
